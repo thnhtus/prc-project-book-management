d025a7da24b56fb11f9bfa64b300f04d1ceaa96e
tubt4 <36790617+thnhtus@users.noreply.github.com>
2021-06-28 00:40:39 +0700

    modify book table

--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -1,48 +1,194 @@
-import { Input, Button } from "antd";
-import React from "react";
-import BookTable from "./data/BookTable";
-import { Link } from "react-router-dom";
-import { useState, useEffect } from 'react';
 import {
-  PlusOutlined
-} from "@ant-design/icons";
-import AddNewBookForm from './AddNewBookForm'
-
+  Input,
+  Button,
+  Table,
+  Divider,
+  Tag,
+  Space,
+  Form,
+  Checkbox,
+} from "antd";
+import React from "react";
+// import BookTable from "./data/BookTable";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { PlusOutlined, EditTwoTone } from "@ant-design/icons";
+import AddNewBookForm from "./AddNewBookForm";
+import axios from "axios";
 
 const BookLists = () => {
+  const [nameSearch, setNameSearch] = useState("");
+
+  //book data
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    await axios
+      .get(`https://bookmanagementapi.azurewebsites.net/api/books/search-books`)
+      .then((res) => {
+        var i = 1;
+
+        setBooks(
+          res.data.map((row) => ({
+            rowIndex: i++,
+            key: row.bookId,
+            title: row.title,
+            category: row.categoryName,
+            author: row.author,
+            price: row.price,
+            amount: row.amount,
+            releaseYear: row.releaseYear,
+            publisher: row.publisher,
+            language: row.languageName,
+          }))
+        );
+      })
+      .then(() => {
+        console.log("Book", books);
+      });
+  };
+
   const { Search } = Input;
   //visible addNewBookForm popup
   const [visible, setVisible] = useState(false);
 
   const onCreate = (values) => {
-    console.log('Received values from form: ', values);
+    console.log("Received values from form: ", values);
     setVisible(false);
   };
+
+  const columns = [
+    {
+      title: "",
+      dataIndex: "rowIndex",
+      key: "rowIndex",
+      width: "1%",
+    },
+    {
+      title: "Book Title",
+      dataIndex: "title",
+      key: "title",
+      width: "25%",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Author",
+      dataIndex: "author",
+      key: "author",
+      // sorter: {
+      //   compare: (a, b) => a.username.length - b.username.length,
+      //   multiple: 3,
+      // },
+    },
+    {
+      title: "Categories",
+      dataIndex: "category",
+      key: "category",
+      render: (category) => (
+        <>
+          {category !== null && (
+            <Tag color="volcano">{category.toUpperCase()}</Tag>
+          )}
+        </>
+      ),
+    },
+    {
+      title: "Languages",
+      dataIndex: "language",
+      key: "language",
+    },
+    {
+      title: "Publisher",
+      dataIndex: "publisher",
+      key: "publisher",
+      render: (publisher) => (
+        <>
+          {publisher == null ? (
+            <span style={{ color: "red" }}>Unkown</span>
+          ) : (
+            <span>{publisher}</span>
+          )}
+          {publisher === "" && <span style={{ color: "red" }}>Unkown</span>}
+        </>
+      ),
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+      render: (text) => <span>${text}</span>,
+    },
+    {
+      title: "Amounts",
+      dataIndex: "amount",
+      key: "amount",
+      render: (text) => (
+        <>
+          {text === 0 ? (
+            <span style={{ fontWeight: 700, color: "red" }}>{text}</span>
+          ) : (
+            <span style={{ fontWeight: 700 }}>{text}</span>
+          )}
+        </>
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      align: "center",
+      render: (record) => (
+        <Space size="middle">
+          <Button type="default" icon={<EditTwoTone />}>
+            Update
+          </Button>
+          {/* <Button type="danger" icon={<DeleteOutlined />}></Button> */}
+        </Space>
+      ),
+    },
+  ];
 
   return (
     <div className="main-content-container">
       <div className="main-content">
         <div className="main-content-row">
           <div className="row-search">
-            <Search
+            <Input.Search
               placeholder="Search..."
               className="search-bar"
               size="large"
               allowClear
-              //onSearch={}
+              // onSearch={(search) => {
+              //   //set nameSearch
+              //   console.log("curr", search);
+              //   setNameSearch(search);
+              //   console.log("ns", nameSearch);
+              //   console.log("BD", books);
+              //   console.log(typeof books);
+
+              //   let i = 1;
+              //   const resultSearch = books.map((row) => ({
+              //     row.title.includes(nameSearch)
+              //   }));
+
+              //   console.log("SD", resultSearch);
+              //   if (resultSearch.length === 0) {
+              //     console.log("Arr Null!");
+              //   }
+              // }}
               style={{ width: 600 }}
               enterButton
             />
-            
           </div>
           <div className="row-btn-add">
-            
-            
-            <Button 
-            type="primary" 
-            size="large" 
-            icon={<PlusOutlined />} 
-            onClick={() => setVisible(true)}
+            <Button
+              type="primary"
+              size="large"
+              icon={<PlusOutlined />}
+              onClick={() => setVisible(true)}
             >
               Add New Book
             </Button>
@@ -64,7 +210,7 @@ const BookLists = () => {
 
         <div className="main-content-row">
           <div className="books-table">
-            <BookTable />
+            <Table dataSource={books} columns={columns} bordered></Table>
           </div>
         </div>
       </div>
--- src/components/data/BookTable.js
+++ src/components/data/BookTable.js
@@ -1,110 +1,164 @@
-import React from "react";
-import { Table, Divider, Tag, Button, Space } from "antd";
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { DeleteOutlined } from '@ant-design/icons';
+// import React from "react";
+// import {
+//   Table,
+//   Divider,
+//   Tag,
+//   Button,
+//   Space,
+//   Form,
+//   Input,
+//   Checkbox,
+// } from "antd";
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+// import { DeleteOutlined, EditTwoTone } from "@ant-design/icons";
 
-const BookTable = () => {
-//book data
-const [books, setBooks] = useState([]);
+// const BookTable = ({ books }) => {
+//   const [dataSource, setDataSource] = useState(books);
 
-useEffect(() => {
-  getData();
-}, [])
+//   // useEffect(() => {
+    
+//   //   setDataSource((books) => {
+//   //     books.map((book) => ({
+//   //       rowIndex: book.rowIndex,
+//   //       key: book.bookId,
+//   //       title: book.title,
+//   //       category: book.categoryName,
+//   //       author: book.author,
+//   //       price: book.price,
+//   //       amount: book.amount,
+//   //       releaseYear: book.releaseYear,
+//   //       publisher: book.publisher,
+//   //       language: book.languageName,
+//   //     }));
+//   //   });
+//   //   console.log("DS", dataSource)
+//   // }, []);
 
-const getData = async () => {
-  await axios.get(`https://bookmanagementapi.azurewebsites.net/api/books/search-books`)
-        .then(res => {
-          setBooks(res.data.map(row => ({
-            key: row.bookId,
-            title: row.title,
-            category: row.categoryName,
-            author: row.author,
-            price: row.price,
-            amount: row.amount,
-            releaseYear: row.releaseYear,
-            publisher: row.publisher,
-            language: row.languageName
-            
-          })))
-        })
-        .then(() => {
-          console.log('Book', books)
-        })
-}
+//   // //book data
+//   // const [books, setBooks] = useState([]);
 
+//   // useEffect(() => {
+//   //   getData();
+//   // }, []);
 
+//   // const getData = async () => {
+//   //   await axios
+//   //     .get(`https://bookmanagementapi.azurewebsites.net/api/books/search-books`)
+//   //     .then((res) => {
+//   //       var i = 1;
 
+//   //       setBooks(
+//   //         res.data.map((row) => ({
+//   //           rowIndex: i++,
+//   //           key: row.bookId,
+//   //           title: row.title,
+//   //           category: row.categoryName,
+//   //           author: row.author,
+//   //           price: row.price,
+//   //           amount: row.amount,
+//   //           releaseYear: row.releaseYear,
+//   //           publisher: row.publisher,
+//   //           language: row.languageName,
+//   //         }))
+//   //       );
+//   //     })
+//   //     .then(() => {
+//   //       console.log("Book", books);
+//   //     });
+//   // };
 
+//   const columns = [
+//     {
+//       title: "",
+//       dataIndex: "rowIndex",
+//       key: "rowIndex",
+//       width: "1%",
+//     },
+//     {
+//       title: "Book Title",
+//       dataIndex: "title",
+//       key: "title",
+//       width: "25%",
+//       render: (text) => <a>{text}</a>,
+//     },
+//     {
+//       title: "Author",
+//       dataIndex: "author",
+//       key: "author",
+//       // sorter: {
+//       //   compare: (a, b) => a.username.length - b.username.length,
+//       //   multiple: 3,
+//       // },
+//     },
+//     {
+//       title: "Categories",
+//       dataIndex: "category",
+//       key: "category",
+//       render: (category) => (
+//         <>
+//           {category !== null && (
+//             <Tag color="volcano">{category.toUpperCase()}</Tag>
+//           )}
+//         </>
+//       ),
+//     },
+//     {
+//       title: "Languages",
+//       dataIndex: "language",
+//       key: "language",
+//     },
+//     {
+//       title: "Publisher",
+//       dataIndex: "publisher",
+//       key: "publisher",
+//       render: (publisher) => (
+//         <>
+//           {publisher == null ? (
+//             <span style={{ color: "red" }}>Unkown</span>
+//           ) : (
+//             <span>{publisher}</span>
+//           )}
+//           {publisher === "" && <span style={{ color: "red" }}>Unkown</span>}
+//         </>
+//       ),
+//     },
+//     {
+//       title: "Price",
+//       dataIndex: "price",
+//       key: "price",
+//       render: (text) => <span>${text}</span>,
+//     },
+//     {
+//       title: "Amounts",
+//       dataIndex: "amount",
+//       key: "amount",
+//       render: (text) => (
+//         <>
+//           {text === 0 ? (
+//             <span style={{ fontWeight: 700, color: "red" }}>{text}</span>
+//           ) : (
+//             <span style={{ fontWeight: 700 }}>{text}</span>
+//           )}
+//         </>
+//       ),
+//     },
+//     {
+//       title: "Action",
+//       key: "action",
+//       align: "center",
+//       render: (record) => (
+//         <Space size="middle">
+//           <Button type="default" icon={<EditTwoTone />}>
+//             Update
+//           </Button>
+//           {/* <Button type="danger" icon={<DeleteOutlined />}></Button> */}
+//         </Space>
+//       ),
+//     },
+//   ];
 
-const columns = [
-  {
-    title: 'Book Title',
-    dataIndex: 'title',
-    key: 'title',
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: 'Author',
-    dataIndex: 'author',
-    key: 'author',
-    // sorter: {
-    //   compare: (a, b) => a.username.length - b.username.length,
-    //   multiple: 3,
-    // },
-  },
-  {
-    title: 'Categories',
-    dataIndex: 'category',
-    key: 'category',
-    render: (category) => (
-      <>
-        {category === 'Romance' && <Tag color="red">{category}</Tag>}
-      </>
-    ),
-  },
-  {
-    title: 'Languages',
-    dataIndex: 'language',
-    key: 'language',
-  },
-  {
-    title: 'Publisher',
-    dataIndex: 'publisher',
-    key: 'publisher',
-    render: (publisher) => (
-      <>
-        {publisher === null ? 
-        (<span style={{ color: "red" }} >Unkown</span>) :
-        (<span >{publisher}</span>)
-        }
-      </>
-    ),
-  },
-  {
-    title: 'Price',
-    dataIndex: 'price',
-    key: 'price',
-    render: text => <p>${text}</p>
-  },
-  {
-    title: 'Amounts',
-    dataIndex: 'amount',
-    key: 'amount',
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: () => (
-      <Space size="middle">
-        <Button type="danger" icon={<DeleteOutlined />}></Button>
-      </Space>
-    ),
-  },
-];
+//   return <Table dataSource={dataSource} columns={columns} bordered></Table>;
+// };
 
-
-  return <Table dataSource={books} columns={columns} bordered ></Table>;
-};
-
-export default BookTable;
+// export default BookTable;
